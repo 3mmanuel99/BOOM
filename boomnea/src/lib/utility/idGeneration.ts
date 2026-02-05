ca8cfa0132d888ad8a6879a4439f34b577655d24
3mmanuel99 <s240338@rochdalesfc.ac.uk>
2026-02-05 10:29:10 +0000

    rewrite of idgen

--- boomnea/src/lib/utility/idGeneration.ts
+++ boomnea/src/lib/utility/idGeneration.ts
@@ -1,53 +1,61 @@
 export class IDGenerators {
-    combination = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-    users = "US-";
-    questions = "QN-";
-    gamecodes = "BM-";
-    messages = "MS-";
+    combination: string;
+    users: string;
+    questions: string;
+    gamecodes: string;
+    messages: string;
 
-
-    static questionIdGenerator(): string
+    constructor()
     {
+        this.combination = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+        this.users = "US-";
+        this.questions = "QN-";
+        this.gamecodes = "BM-";
+        this.messages = "MS-";
+    }
 
-        this.
+
+    public questionIdGenerator(): string
+    {
          
         for (let i = 0; i < 10; i++)
         {
             /* adding each character from the string into the variable
             the concat function combines multiple characters into a single string
             */
-            IDusers += "".concat(this.combination[Math.floor(Math.random() * combination.length)]);
+            this.questions += "".concat(this.combination[Math.floor(Math.random() * this.combination.length)]);
             /* Math.floor() will return a whole number, while Math.random() will return a number between 0 and 1.
             the former is needed as you cannot access a position in an array with a fractional number. the multi-
             plication step comes in as it steps up the range from 0 and 1 to a much larger range, putting it from 0
             to the length of the string combination (62).
             */
         }
-        return id; // returning the id that has been generated
-    }
-    static userIdGeneration(): string {
-        const combination: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-        let id: string = "US-";  
-        for (let i = 0; i < 10; i++)
-        {
-            id += "".concat(combination[Math.floor(Math.random() * combination.length)]);
-        }
-        return id; 
-    }
-    static gamecodeIdGeneration(): string {
-        const combination: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-        let gamecode: string = "BM-";
-
-        // instead of looping 10 times like we are doing for other id generators, we will be looping only 5 for the convenience of the user(s)
-        for (let i = 0; i < 10; i++)
-        {
-            gamecode += "".concat(combination[Math.floor(Math.random() * combination.length)])
-        }
-        return gamecode; // returning the gamecode that has been generated
+        return this.questions; // returning the id that has been generated
     }
 
-    static messageIdGeneration(): string {
-        const combination = string "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-        let gamecode: string = 
+
+    public userIdGeneration(): string {
+        for (let i = 0; i < 10; i++)
+        {
+            this.users += "".concat(this.combination[Math.floor(Math.random() * this.combination.length)]);
+        }
+        return this.users; 
+    }
+    public gamecodeIdGeneration(): string {
+      // instead of looping 10 times like we are doing for other id generators, we will be looping only 5 for the convenience of the user(s)
+        for (let i = 0; i < 10; i++)
+        {
+            this.gamecodes += "".concat(this.combination[Math.floor(Math.random() * this.combination.length)])
+        }
+        return this.gamecodes; // returning the gamecode that has been generated
+    }
+
+    public messageIdGeneration(): string {
+        for (let i = 0; i < 10; i++)
+        {
+            this.messages = "".concat(this.combination[Math.floor(Math.random() * this.combination.length)])
+        }
+
+        return this.messages;
     }
 }
